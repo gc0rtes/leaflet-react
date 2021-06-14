17c26cdd4252fa046c49f0ff51bf06e199f8f60f
gc0rtes <guilhermeco@gmail.com>
2021-06-14 08:57:04 +0200

    map added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-function App() {
+import React, { Component } from "react";
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+//it has to be a class
+export default function App() {
+  const myCoord = [52.05579, 4.28593]; //zuiderpark (pick from google)
   return (
-    <div className="App">
-      <p>Hello World!</p>
+    <div className="container">
+      <MapContainer
+        className="myMap"
+        center={myCoord}
+        zoom={15}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution="ZuiderPark"
+          // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={myCoord}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
-
-export default App;
